6cdf39e4969e9e6774bea7510f15bd99247192a6
Ben Ross <mrbenross@gmail.com>
2023-12-23 15:05:46 -0600

    final commit

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -40,20 +40,33 @@ router.put('/:id', validateUserId,validateUser, (req, res,next) => {
   .catch(next)
 });
 
-router.delete('/:id', (req, res) => {
-  // RETURN THE FRESHLY DELETED USER OBJECT
-  // this needs a middleware to verify user id
+router.delete('/:id',validateUserId, async (req, res, next) => {
+  await User.remove(req.params.id)
+  try{
+     res.json(req.user)
+  } catch(err){
+   next(err)
+  }
 });
 
-router.get('/:id/posts', (req, res) => {
-  // RETURN THE ARRAY OF USER POSTS
-  // this needs a middleware to verify user id
+router.get('/:id/posts', validateUserId, (req, res, next) => {
+  User.getUserPosts(req.user.id)
+  .then(post =>{
+    res.json(post)
+  })
+  .catch(next)
 });
 
-router.post('/:id/posts', (req, res) => {
-  // RETURN THE NEWLY CREATED USER POST
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+router.post('/:id/posts', validateUserId,validatePost,async (req, res, next) => {
+  try{
+    const result = await Post.insert({
+     user_id: req.params.id,
+     text: req.text
+    })
+    res.json(result)
+  } catch(err){
+   next(err)
+  }
 });
 
 
